e528f0b03694c1209dd9c10186d6354cd2c889e2
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-21 09:42:47 +0300

    add handle cors

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-import json
-from flask import Flask, request
-import io, os
+import io
 from contextlib import redirect_stdout
-from flask_cors import CORS
-
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
-CORS(app)
 
-def serialize(data):
-    return json.dumps(data).replace("null", "None")
-
-@app.route('/runCode', methods=['POST'])
+@app.route('/runCode', methods=['POST', 'OPTIONS'])
 def runCode():
-    script = request.json['script']
-    input_data = request.json['input']
-    print("run code called")
+    if request.method == "OPTIONS":
+        return build_cors_preflight_response()
+    elif request.method == "POST":
+        script = request.json['script']
+        input_data = request.json['input']
+        print("run code called")
 
-    output_buffer = io.StringIO()
-    with redirect_stdout(output_buffer):
-        try:
-            exec(script, {"input": lambda: input_data})
-            massage = "Script executed successfully"
+        output_buffer = io.StringIO()
+        with redirect_stdout(output_buffer):
+            try:
+                exec(script, {"input": lambda: input_data})
+            except Exception as e:
+                print(str(e))
 
-        except Exception as e:
-            print(str(e))
-            massage=str(e)
+        output = output_buffer.getvalue()
+        print("run code return output")
+        response = jsonify({"output": output})
+        return corsify_actual_response(response)
 
-    output = output_buffer.getvalue()
+def build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
-    print("run code return output")
-    return {"output": output, "massage":massage}
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+def corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
