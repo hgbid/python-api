85755cd60429e32a43f527c016e77a728638c194
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-21 15:15:36 +0300

    define the port

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ def corsify_actual_response(response):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000)
