6ea5b2372aa323549b63be43dcd54d1b0e7a988a
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-21 09:20:24 +0300

    port changed

--- main.py
+++ main.py
@@ -31,4 +31,6 @@ def runCode():
     return {"output": output, "massage":massage}
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
